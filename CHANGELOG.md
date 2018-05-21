6715265e88b88c25d74261c60150c1ab2f230423
sphinxxxx <coding@enthuses.me>
2018-05-21 20:40:33 +0000

    Added a CHANGELOG and disabled package-lock.json

---
+++ CHANGELOG.md
@@ -0,0 +1,29 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
+and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
+
+
+## [Unreleased]
+
+
+## [0.1.1] - 2018-05-21
+### Added
+- This CHANGELOG file.
+
+### Removed
+- Disabled package-lock.json (https://stackoverflow.com/questions/44206782/do-i-commit-the-package-lock-json-file-created-by-npm-5).
+
+
+## [0.1.0] - 2018-03-26
+### Added
+- Prepublish/postpublish scripts.
+
+
+## 0.0.0 - 2018-03-24
+
+
+[Unreleased]: https://github.com/Sphinxxxx/gulp-rollup-boilerplate/compare/v0.1.1...HEAD
+[0.1.1]: https://github.com/Sphinxxxx/gulp-rollup-boilerplate/compare/v0.1.0...v0.1.1
+[0.1.0]: https://github.com/Sphinxxxx/gulp-rollup-boilerplate/compare/v0.0.0...v0.1.0
--- dist/gulp-rollup-boilerplate.js
+++ dist/gulp-rollup-boilerplate.js
@@ -1,20 +1,20 @@
 /*!
- * gulp-rollup-boilerplate v0.0.0
+ * gulp-rollup-boilerplate v0.1.1
  * https://github.com/Sphinxxxx/gulp-rollup-boilerplate
  *
  * Copyright 2018 boilerplate-author
  * Released under the MIT license.
  */
 (function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-	typeof define === 'function' && define.amd ? define(factory) :
-	(global.boilerplateLib = factory());
+    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+    typeof define === 'function' && define.amd ? define(factory) :
+    (global.boilerplateLib = factory());
 }(this, (function () { 'use strict';
 
-function boilerplateLib() {
-    return "BOILER!";
-}
+    function boilerplateLib() {
+        return "BOILER!";
+    }
 
-return boilerplateLib;
+    return boilerplateLib;
 
 })));
